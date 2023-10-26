c6ff362fa990f3e196dba4f342d4c71145367cc7
vankoro <ki-2004@yandex.ru>
2023-10-26 10:22:41 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.setAge(56);
+        person1.setName("Иван");
+
+
+
+
+        person1.move(20);
+        person1.talk("Hello everyone");
+
+    }
+}
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,49 @@
+package org.example;
+
+public class Person {
+    private String name;
+    private String surname;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public Person(String name, String surname, Integer age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public void move(int countSteps) {
+        System.out.println(name + " сделал " + countSteps + " шагов");
+    }
+
+    public void talk(String s) {
+        System.out.println(name + " говорит: " + s);
+    }
+
+}
